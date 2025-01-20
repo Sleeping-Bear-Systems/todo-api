bd7844c1b1fc209436718ee84066e926f23a1ee7
Charles Farris <charlesfarris71@gmail.com>
2025-01-20 10:10:09 -0700

    Add ping feature

---
+++ src/WebApi/Features/Ping.cs
@@ -0,0 +1,18 @@
+﻿namespace SleepingBear.ToDo.WebApi.Features;
+
+/// <summary>
+/// Ping feature.
+/// </summary>
+internal static class Ping
+{
+    /// <summary>
+    /// Maps the "Ping" endpoint.
+    /// </summary>
+    // ReSharper disable once UnusedMethodReturnValue.Global
+    public static IEndpointRouteBuilder MapPing(this IEndpointRouteBuilder endpoints)
+    {
+        endpoints.MapGet("/api/ping", () => Results.Ok());
+
+        return endpoints;
+    }
+}
--- src/WebApi/Program.cs
+++ src/WebApi/Program.cs
@@ -1,3 +1,5 @@
+using SleepingBear.ToDo.WebApi.Features;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -13,5 +15,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.MapPing();
 
 app.Run();
