9bbfab54315b218d8c0a630a742ee4a564cc5d28
Charles Farris <charlesfarris71@gmail.com>
2025-01-20 10:46:23 -0700

    Add DbContext
    - Add DI registration

---
+++ src/WebApi/Common/CommonExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SleepingBear.ToDo.WebApi.Common.Database;
+
+namespace SleepingBear.ToDo.WebApi.Common;
+
+internal static class CommonExtensions
+{
+    /// <summary>
+    /// Register the Common services.
+    /// </summary>
+    public static IServiceCollection AddCommon(
+        this IServiceCollection services,
+        TimeProvider timeProvider,
+        string connectionString)
+    {
+        services.AddSingleton(timeProvider);
+        services.AddDbContext<ToDoDbContext>(options => options.UseNpgsql(connectionString));
+        return services;
+    }
+}
--- src/WebApi/Program.cs
+++ src/WebApi/Program.cs
@@ -1,10 +1,15 @@
+using SleepingBear.ToDo.WebApi.Common;
 using SleepingBear.ToDo.WebApi.Features;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var connectionString = string.Empty;
+
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddCommon(TimeProvider.System, connectionString);
 
 var app = builder.Build();
 
