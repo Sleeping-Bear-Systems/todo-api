f13886f3991b97032f671e6e2f9e7f1287636004
Charles Farris <charlesfarris71@gmail.com>
2025-01-20 10:23:02 -0700

    Format code

--- src/WebApi/Features/Ping.cs
+++ src/WebApi/Features/Ping.cs
@@ -1,12 +1,12 @@
 ﻿namespace SleepingBear.ToDo.WebApi.Features;
 
 /// <summary>
-/// Ping feature.
+///     Ping feature.
 /// </summary>
 internal static class Ping
 {
     /// <summary>
-    /// Maps the "Ping" endpoint.
+    ///     Maps the "Ping" endpoint.
     /// </summary>
     // ReSharper disable once UnusedMethodReturnValue.Global
     public static IEndpointRouteBuilder MapPing(this IEndpointRouteBuilder endpoints)
